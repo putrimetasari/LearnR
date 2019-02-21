692f404b76b7f48f10310e0faaca585bd9d54d10
putrimetasari <47780084+putrimetasari@users.noreply.github.com>
2019-02-21 22:13:59 +0700

    Add files via upload

---
+++ BasicR.R
@@ -0,0 +1,19 @@
+v_hobi_saya = c("makan","membaca","bernyanyi")
+
+ganjil = c(1,3,5,7,9,11)
+matrix_ganjil_saya = matrix(c(ganjil),byrow=T,nrow=2)
+matrix_ganjil_saya
+
+bmi = data.frame(
+  makanan = c("Pizza","Spaghetti","Soto Ayam"),
+  harga=c(25000,45000,30000)
+)
+
+head(bmi)
+
+#list
+my.list = list(matrix_ganjil_saya,bmi)
+my.list
+
+
+
